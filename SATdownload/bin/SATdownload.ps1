290c3e9b324befa5d8ca6e36a1aad8f0c90094fc
bmoon-scu <bmoon@scu.edu>
2015-11-12 11:49:57 -0800

    Renamed "SATdownload" to "SATdownload (Java)"

--- SATdownload/bin/SATdownload.ps1
+++
@@ -1,9 +0,0 @@
-#
-# Script to aid in the execution of SATdownload.jar
-#
-
-$CONFIG_FILE="config.properties"
-$BIN_DIR=Split-Path -parent $MyInvocation.MyCommand.Source
-
-cd $BIN_DIR
-java -jar SATdownload.jar --config=$CONFIG_FILE

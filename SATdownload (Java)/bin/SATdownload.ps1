3fa6004204d0211e4a0144e1d198a28bcf1ee0c1
Brian Moon <bmoon@scu.edu>
2015-11-15 11:01:52 -0800

    Version 1.1

--- SATdownload (Java)/bin/SATdownload.ps1
+++ SATdownload (Java)/bin/SATdownload.ps1
@@ -2,8 +2,9 @@
 # Script to aid in the execution of SATdownload.jar
 #
 
-$CONFIG_FILE="config.properties"
+$CONFIG_FILE=SATdownload.conf
+$VERSION=1.1
 $BIN_DIR=Split-Path -parent $MyInvocation.MyCommand.Source
 
 cd $BIN_DIR
-java -jar SATdownload.jar --config=$CONFIG_FILE
+java -jar SATdownload-$VERSION.jar --config=$CONFIG_FILE
--- SATdownload (Java)/bin/SATdownload.sh
+++ SATdownload (Java)/bin/SATdownload.sh
@@ -3,9 +3,10 @@
 # Script to aid in the execution of SATdownload.jar
 #
 
-CONFIG_FILE=config.properties
+CONFIG_FILE=SATdownload.conf
+VERSION=1.1
 JAVA=/usr/bin/java
 BIN_DIR=$(/usr/bin/dirname $0)
 
 cd $BIN_DIR
-$JAVA -jar SATdownload.jar --config=$CONFIG_FILE
+$JAVA -jar SATdownload-${VERSION}.jar --config=$CONFIG_FILE
